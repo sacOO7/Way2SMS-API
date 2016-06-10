ade952d5bee06bd3267805aedb75796413a8eed8
sachin <sachinshinde7676@gmail.com>
2016-06-10 22:51:17 +0530

    Added Copyright

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,3 +1,21 @@
+
+
+/*
+ * Copyright 2016 sachin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.company;
 
 import com.jaunt.NotFound;
@@ -12,8 +30,8 @@ public class Main {
         Way2SMS way2SMS = new Way2SMS();
         way2SMS.Login(USERNAME,PASSWORD);
 
-        for (int i=0;i<4;i++) {
+//        for (int i=0;i<4;i++) {
             way2SMS.SendSMS("", ""); //args ; Phone no , Message
-        }
+//        }
     }
 }
